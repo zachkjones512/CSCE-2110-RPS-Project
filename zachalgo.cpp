e0d594dc3b319c54473cb1810eeeeff5df6d4881
Jones, Zachary <zacharyjones4@my.unt.edu>
2022-11-28 18:01:05 -0600

    Created initial algorithm

--- zachalgo.cpp
+++ zachalgo.cpp
@@ -1,14 +1,44 @@
 #include <string>
 #include <iostream>
+#include <cstdlib>
+#include <time.h>
 
 using namespace std;
 
-void zachAlgo(){
+char zachmove(int rounds, string oppmoves, string &mymoves;){ //this algorithm attempts to find a pattern in my opponents moves by attempting to see if the last five moves had been played prior
+    char movelist[3] = {'R', 'P', 'S'}; 
+    srand(time(NULL));
 
 
+    if(rounds < 6){
+        int randmove = rand()%3
+        mymoves = mymoves + movelist[randmove] //randomizes the first six rounds of play(before algorithm can make predictions)
+        return movelist[randmove];
+    }
+    else{ //if its round 7 or above
+        
+        string lastfive = oppmoves.substr(rounds-5, 5); //create a substring of the last five moves
+        size_t found = oppmoves.find(lastfive); //find the position of the substring in the list of opponent moves
+        if (found != string::npos){ //if the substring is found, read the following move and then counter it
+            if (oppmoves[found+5] == 'R'){
+            mymoves = mymoves + 'P';
+            }
+            if (oppmoves[found+5] == 'P'){
+            mymoves = mymoves + 'S';
+            }
 
+            if (oppmoves[found+5] == 'S'){
+            mymoves = mymoves + 'R';
+            return 'R';
+            }
 
-
-
+        }
+        else{ //if the string is not found, return a random move
+            
+            int randmove = rand()%3
+            mymoves = mymoves + movelist[randmove]
+            return movelist[randmove];
+        }
+    }
     
 }
