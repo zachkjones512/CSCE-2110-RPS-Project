e3de90bbf8237ff7743ecdbd80449448d81dc9eb
Kaderli, Daniel <DanielKaderli@my.unt.edu>
2022-12-01 02:16:47 -0600

    Added comments and cleaned up code

--- drkFreq.cpp
+++ drkFreq.cpp
@@ -1,9 +1,50 @@
+/*  Name: Daniel Kaderli
+    Date: 11/30/22
+    Program: RPS Frequency Algorithm
+    Description: Algorith that uses the frequency of the opponents 
+    previous moves to predict the most likely move and counter it.
+    Variables: 
+        - rockSum, paperSum, scissorSum:
+            These integers are used to keep track of the number
+            of times the opponent uses each move. They are used
+            to calculate the frequencies of each move.
+        - rockFreq, paperFreq, scissorFreq:
+            These floats are used to keep track of the frequency
+            of each move used by the opponent. They are calculated
+            by dividing the corresponding move's sum by the total
+            number of moves.
+    
+    Functions:
+        - SelectRand()
+            This function selects a random move. It's called
+            whenever the previous move is invalid or the 
+            frequencies are all equal.
+        
+        - PrintStats()
+            Debugging function that prints out the sums and 
+            frequencies of each move.
+        
+        - FrequencyFinder()
+            Adds the previous move to the sum variable of its
+            move. Then it gets the frequency by dividing the
+            sum from the total number of moves.
+        
+        - ChooseMove()
+            This function checks to see if the move is valid
+            then adds it to the list and calls the
+            FrequencyFinder Function to choose the move.
+
+    Notes to self:
+        Add simple algorithms to debug against
+
+
+*/
+
 #include "drkFreq.h"
 
-//char prevMoves[5] = {'R', 'R', 'R', 'R', 'R'};
-
+// Function for selecting random move
 char Frequency::SelectRand(){
-    int randMove = rand() % 3;
+    int randMove = rand() % 3; // random num between 0 and 2
     switch(randMove){
             case 0:
                 return 'R';
@@ -17,6 +58,7 @@ char Frequency::SelectRand(){
     }
 }
 
+// function to print stats for debugging
 void Frequency::PrintStats(){
     cout << "Sums" << endl;
     cout << "Rock: " << rockSum << endl;
@@ -29,30 +71,29 @@ void Frequency::PrintStats(){
     cout << "Scissors: " << scissorFreq << endl;
 }  
 
-char Frequency::ChooseMove(char prevMove){
+// function that calculates the frequencies of each move
+void Frequency::FrequencyFinder(char prevMove){
 
     //cout << moves << endl;
-    if(prevMove == '|'){
-        return SelectRand();
-    }else{
-        moves += prevMove;
-        switch(prevMove){
-            case 'R':
-                rockSum++;
-                break;
-            
-            case 'P':
-                paperSum++;
-                break;
+     
 
-            case 'S':
-                scissorSum++;
-                break;
-            
-            default:
-                cout << "invalid move" << endl;
-                break;
-        }
+        // increases sum variables
+    switch(prevMove){
+        case 'R':
+            rockSum++;
+            break;
+        
+        case 'P':
+            paperSum++;
+            break;
+
+        case 'S':
+            scissorSum++;
+            break;
+        
+        default:
+            cout << "invalid move" << endl;
+            break;
     }
 
 
@@ -62,8 +103,20 @@ char Frequency::ChooseMove(char prevMove){
     paperFreq = ((float)paperSum/(float)moves.length());
     scissorFreq = ((float)scissorSum/(float)moves.length());
 
+}
 
+// function that chooses move based on frequencies
+char Frequency::ChooseMove(char prevMove){
+
+    // prevents | from being added to moves
+    if(prevMove == '|'){
+        return SelectRand();
+    }else{ 
+        moves += prevMove; // adds each move to the list
+        FrequencyFinder(prevMove); // finds frequencies
+    }
     
+    // decides which move to choose based on previous frequencies
     if(rockFreq > paperFreq && rockFreq > scissorFreq){
         return 'P';
     }
@@ -75,7 +128,6 @@ char Frequency::ChooseMove(char prevMove){
     if(scissorFreq > paperFreq && scissorFreq > rockFreq){
         return 'R';
     }
-
-    
+  
     return SelectRand();
 }
--- drkFreq.h
+++ drkFreq.h
@@ -3,18 +3,21 @@
 using namespace std;
 
 class Frequency{
-    // variables for move tracking
+    // frequencies of each move's occurance
     float rockFreq;
     float paperFreq;
     float scissorFreq;
 
+    // number of times each move played by opponent
     int rockSum;
     int paperSum;
     int scissorSum;
 
+    // stores moves as a string
     string moves;
 
     public:
+    // constructor
     Frequency(){
         rockFreq = 0.00;
         paperFreq = 0.00;
@@ -24,9 +27,11 @@ class Frequency{
         paperSum = 0;
         scissorSum = 0;
     }
+    // functions for algorithm (see drkFreq.cpp for more info)
     char ChooseMove(char);
     char SelectRand();
     void PrintStats();
+    void FrequencyFinder(char);
 
 
 
