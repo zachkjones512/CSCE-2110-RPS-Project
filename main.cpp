8a2ade7ba479a4823ac59b02df712d9f4a4999f5
Caleb Smay <calebsmay@my.unt.edu>
2022-12-05 20:51:20 -0600

    updated post-match analysis

--- main.cpp
+++ main.cpp
@@ -49,12 +49,30 @@ int RPS_Judge(char playerOne, char playerTwo, bool printMessage){
     return 0;
 }
 void RPS_Analysis(int num_rounds, int player_1_wins, int ties){ //TODO: Finish analysis function
+    int player_1_losses = num_rounds - (player_1_wins + ties);
     std::cout << "Round " << num_rounds << std::endl;
-    std::cout << "Win: " << player_1_wins << ", " << player_1_wins / num_rounds << std::endl;
-    std::cout << "Tie: " << ties << ", " << ties / num_rounds << std::endl;
-    std::cout << "Loss: " << num_rounds - (player_1_wins + ties) << ", " << (num_rounds - (player_1_wins + ties)) / num_rounds << std::endl;
+    std::cout << "Win: " << player_1_wins << ", " << (float)(player_1_wins / num_rounds) * 100 << std::endl;
+    std::cout << "Tie: " << ties << ", " << (float)(ties / num_rounds) * 100 << std::endl;
+    std::cout << "Loss: " << player_1_losses << ", " << (float)(player_1_losses / num_rounds) * 100 << std::endl;
 
-    std::cout << "Winning Algorithm:";
+    
+    if(player_1_wins > player_1_losses){
+        std::cout << "Winning Algorithm: agent1" << std::endl;
+        std::cout << "Win Percentage: " << player_1_wins / num_rounds << std::endl;
+        std::cout << "Tie Percentage: " << ties / num_rounds << std::endl;
+
+        std::cout << "Losing Algorithm: agent2";
+        std::cout << "Win Percentage: " << player_1_losses / num_rounds << std::endl;
+        std::cout << "Tie Percentage: " << ties / num_rounds << std::endl;
+    }else{
+        std::cout << "Winning Algorithm: agent2";
+        std::cout << "Win Percentage: " << player_1_losses / num_rounds << std::endl;
+        std::cout << "Tie Percentage: " << ties / num_rounds << std::endl << std::endl;
+
+        std::cout << "Winning Algorithm: agent1" << std::endl;
+        std::cout << "Win Percentage: " << player_1_wins / num_rounds << std::endl;
+        std::cout << "Tie Percentage: " << ties / num_rounds << std::endl;
+    }
 }
 
 int main(){
